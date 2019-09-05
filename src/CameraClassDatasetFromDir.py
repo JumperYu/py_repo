304861db19edabf7964da073689def171eb7f3c5
xiangmin.zeng <xiangmin.zeng@vistel.cn>
2019-09-05 14:58:11 +0800

    add

--- src/CameraClassDatasetFromDir.py
+++ src/CameraClassDatasetFromDir.py
@@ -1,33 +1,41 @@
-import os
+import pandas as pd
+from torch.utils.data import DataLoader
 
-from torch.utils.data.dataset import Dataset  # For custom datasets
+from torch.utils.data.dataset import Dataset  # For custom dataset
 
 
 class CameraClassDatasetFromDir(Dataset):
-    def __init__(self, dir_path):
 
+    def __init__(self, root_path, csv_path, train=True, augment=None):
+        self.root_path = root_path
+        self.train = train
+        self.augment = augment  # 是否需要图片增强
+
+        self.dict = {'0_little_round': 0, '1_triangle': 1, '2_none': 2, '3_big_round': 3, '4_square': 4,
+                     '5_right_round': 5}
+
+        df = pd.read_csv(csv_path)
+
+        self.df = df
         self.label_arr = []
         self.image_arr = []
 
-        for root, dirs, files in os.walk(dir_path):
-            if root == dir_path:
-                continue
+        for index, row in df.iterrows():
+            self.label_arr.append(self.dict[row['className']])
+            self.image_arr.append(row['fileName'])
 
-            for index, file in enumerate(files):
-                # # 获取文件所属目录
-                self.label_arr.insert(index, root[root.rfind("\\") + 1:])
-                # # 获取文件路径
-                self.image_arr.insert(index, os.path.join(root, file))
-
-        print(self.label_arr)
-        print(self.image_arr)
+        self.data_len = len(self.image_arr)
 
     def __getitem__(self, index):
-        return ()
+        data = (self.label_arr[index], self.image_arr[index])
+        return data
 
     def __len__(self):
         return len(self.image_arr)
 
 
 if __name__ == "__main__":
-    cameraClassDatasetFromDir = CameraClassDatasetFromDir('C:\\work\\dev\\camera_class_win10\\')
+    dataset = CameraClassDatasetFromDir('C:/work/dev/camera_class_win10/', '../data/manifest.csv')
+    data_loader = DataLoader(dataset, batch_size=16, shuffle=False)
+    for i, data in enumerate(data_loader):
+        print(data)
